657e54b69b547f03dc89cb4875451fe342501042
maximpiorischin@gmail.com <maximpiorischin@gmail.com>
2020-09-21 22:24:25 -0400

    Added haven monitor with proxies

--- programs/Proxyhaven.py
+++ programs/Proxyhaven.py
@@ -6,16 +6,18 @@ from discord_webhook import DiscordWebhook
 havenrunning = True
 # ------------------
 havenlink = 'https://shop.havenshop.com/collections/new-arrivals'
-allwebhook = 'https://discordapp.com/api/webhooks/751671463660093520/MiMV4BA4qldw2omwVI-37AI_G3eWDIaaRlZKMCX192zpqxudfVLdR2NbZn9-28HrjyiC'
+allwebhook = 'https://discordapp.com/api/webhooks/757788317243932754/_MQtbVKwX7NltbVj5wXdgcujUV20S7KPqFQ27wC8-sdTeKhaCQaZSO_g5xWNhJKam8aA'
+proxies = {'https': 'https://maxsnkrs252165:08621ab5cb7ffe02111Aa@ca.slashproxies.io:20000/',
+           'http': 'http://maxsnkrs252165:08621ab5cb7ffe02111Aa@ca.slashproxies.io:20000/'}
 print("Starting to monitor!")
 if havenrunning:
-    hav = requests.get(havenlink)
+    hav = requests.get(havenlink, proxies)
     havensoup = BeautifulSoup(hav.content, 'html.parser')
     haven_list = havensoup.find_all(class_='product-card-name')
 # ----------------------------------------------------------------------------------------
 def siteupdatehaven():
     global haven_list
-    n = requests.get(havenlink)
+    n = requests.get(havenlink, proxies)
     tempsoup = BeautifulSoup(n.content, 'html.parser')
     temp_list = tempsoup.find_all(class_='product-card-name')
     for item in temp_list:
@@ -34,3 +36,4 @@ while True:
     time.sleep(5)
     if havenrunning:
         siteupdatehaven()
+    print('w')
